b3983cc08e92a9eb7628fa0781cc2224cb9f3f05
doranj-wcsu <112014309+doranj-wcsu@users.noreply.github.com>
2022-09-07 12:18:53 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask import Flask,jsonify,request
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World Jim!"
+
+  
+@app.route('/returnjson', methods = ['GET'])
+def ReturnJSON():
+    if(request.method == 'GET'):
+        data = {
+            "Modules" : 15,
+            "Subject" : "Data Structures and Algorithms",
+        }
+  
+        return jsonify(data)
+
+
+
+if __name__ == '__main__':
+     app.run()   
+#     app.run(host="localhost",  port=8080 ,debug=True)
+
+    
